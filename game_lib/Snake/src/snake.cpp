fc3548a13149e9bfbe7cae1fb21ef7727e39a28e
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-03-18 20:50:04 +0100

    add: snake can now eat an item and grown up of 1 part of his body, when he eat we replace the latest part of this body by a tail and the n-1 who was the tail is now a part of his body, new screen of end when the user loose

--- game_lib/Snake/src/snake.cpp
+++ game_lib/Snake/src/snake.cpp
@@ -63,21 +63,21 @@ void Arc::Snake::createTile(Pos pos, Size sizeTile, TypeOfTile type)
             tile.color = Arc::Color::GREEN;
             tile.c = ' ';
         break;
-        case FOOD:
-            tile.imagePath = std::string(PATH_IMG) + "apple.png";
-            tile.color = Arc::Color::RED;
-            tile.c = '@';
-        break;
     }
     tile.size = sizeTile;
     tile.pos = pos;
     this->gameData.tileSet.push_back(tile);
 }
 
+void Arc::Snake::changePartSnake()
+{
+    this->gameData.player.tileSet.back().imagePath = std::string(PATH_IMG) + "body.png";
+    this->gameData.player.tileSet.back().c = '=';
+}
+
 void Arc::Snake::createPlayer(Pos pos, Size sizeTile, SnakeBody snakeBody)
 {
     Tile tile;
-    SnakeMove snakeMove;
 
     switch (snakeBody) {
         case SNAKE_HEAD:
@@ -93,12 +93,11 @@ void Arc::Snake::createPlayer(Pos pos, Size sizeTile, SnakeBody snakeBody)
             tile.c = '2';
             break;
     }
-    snakeMove.snakeBody = snakeBody;
-    snakeMove.pos = pos;
     tile.color = Arc::Color::CYAN;
     tile.size = sizeTile;
     tile.pos = pos;
-    _snakeMove.push_back(snakeMove);
+
+    _snake.push_back(pos);
     this->gameData.player.tileSet.push_back(tile);
 }
 
@@ -128,43 +127,43 @@ void Arc::Snake::createSnake()
         } else {
             createPlayer(this->gameData.tileSet[middleMap + i].pos, Arc::Size(40, 40), SNAKE_BODY);
         }
+        _listDirection.push_front(RIGHT);
     }
+}
+
+Arc::Pos Arc::Snake::computeNewSizeItem()
+{
+    Pos pos;
+
+    pos.x = getRandomPos(540, 1260);
+    pos.y = getRandomPos(140, 820);
+    return pos;
 }
 
 void Arc::Snake::createApple()
 {
-    // Checker si la pomme n'est pas sur le serpent
-    double randomX = getRandomPos(540, 1260);
-    double randomY = getRandomPos(140, 820);
-    createTile(Arc::Pos(randomX, randomY), Arc::Size(SIZE_BORDER, SIZE_BORDER), FOOD);
+    Item item;
+
+    item.pos = computeNewSizeItem();
+
+    item.tile.imagePath = std::string(PATH_IMG) + "item.png";
+    item.tile.color = Arc::Color::RED;
+    item.tile.c = '@';
+    item.tile.size = Arc::Size(40, 40);
+    item.tile.pos = item.pos;
+    this->gameData.item.push_back(item);
+    //this->gameData.tileSet.push_back(item.tile);
 }
 
-Arc::Pos Arc::Snake::findBoxPos(std::size_t idxSearch)
+void Arc::Snake::createAllTexts()
 {
-    Pos pos;
-
-    for (std::size_t i = 0; i < idxSearch; ++i) {
-        if (i % SIZE_MAP == 0) {
-            pos.x += 1;
-            pos.y = 0;
-        } else {
-            pos.y += 1;
-        }
-    }
-    return pos;
-}
-
-void Arc::Snake::init()
-{
-    this->gameData.player.health = 100;
-    std::srand(static_cast<unsigned int>(std::time(nullptr)));
-
-    // TODO Create the 3 texts
     createText("Username: " + this->gameData.player.userName, Arc::Pos(400, 50));
     createText("Actual score: " + std::to_string(_actualScore), Arc::Pos(700, 50));
     createText("High score: " + std::to_string(_highScore), Arc::Pos(1000, 50));
+}
 
-    // TODO Create the map (10 x 10)
+void Arc::Snake::createMap()
+{
     double posX = X_POS_MAP;
     double posY = Y_POS_MAP;
 
@@ -182,7 +181,15 @@ void Arc::Snake::init()
         posX = X_POS_MAP;
         posY += SIZE_BORDER;
     }
+}
 
+void Arc::Snake::init()
+{
+    this->gameData.player.health = 100;
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+    createAllTexts();
+    createMap();
     createSnake();
     createApple();
 
@@ -199,7 +206,7 @@ void Arc::Snake::init()
             - Only handle key LEFT or key RIGHT
     */
 
-    _clockMove.setCooldown(std::chrono::milliseconds(1000));
+    _clockMove.setCooldown(std::chrono::milliseconds(100));
     _clockMove.start();
 }
 
@@ -208,96 +215,76 @@ void Arc::Snake::stop()
     std::cout << "Snake is stopped.\n";
 }
 
-void Arc::Snake::moveSnakeUp()
+void Arc::Snake::changeDirection(NextDirection nextDir)
 {
-    if (_snakeMove.at(0).nextDirection != DOWN) {
-        _snakeMove.at(0).nextDirection = UP;
+    if (nextDir == UP && _direction != DOWN) {
+        _direction = nextDir;
     }
-}
-
-void Arc::Snake::moveSnakeDown()
-{
-    if (_snakeMove.at(0).nextDirection != UP) {
-        _snakeMove.at(0).nextDirection = DOWN;
+    if (nextDir == DOWN && _direction != UP) {
+        _direction = nextDir;
     }
-}
-
-void Arc::Snake::moveSnakeLeft()
-{
-    if (_snakeMove.at(0).nextDirection != RIGHT) {
-        _snakeMove.at(0).nextDirection = LEFT;
+    if (nextDir == RIGHT && _direction != LEFT) {
+        _direction = nextDir;
     }
-}
-
-void Arc::Snake::moveSnakeRight()
-{
-    if (_snakeMove.at(0).nextDirection != LEFT) {
-        _snakeMove.at(0).nextDirection = RIGHT;
+    if (nextDir == LEFT && _direction != RIGHT) {
+        _direction = nextDir;
     }
 }
 
 void Arc::Snake::moveNextCase()
 {
-    /*
-        for (all_part_of_snake) {
-            if (nextDirection(i - 1) != nextDirection(i)) {
-                if (posExpected(i - 1) == pos (i - 1)) {
-                    nextDirection(i) = nextDirection.at(i -1)
-                }
-            }
-        }
-    */
+    Pos new_head;
 
     if (!_clockMove.isElapsed()) {
         return;
     }
     _clockMove.reset();
-    _directionHasChanged = false;
-
-    for (std::size_t i = 0; i < _snakeMove.size(); ++i) {
-        if (i != 0 && _directionHasChanged == false) {
-            _snakeMove.at(i).nextDirection = _snakeMove.at(i - 1).nextDirection;
-            _directionHasChanged = true;
-        }
-
-        switch (_snakeMove.at(i).nextDirection) {
-            case UP:
-                _snakeMove.at(i).pos.y -= 10;
-                break;
-            case DOWN:
-                _snakeMove.at(i).pos.y += 10;
-                break;
-            case LEFT:
-                _snakeMove.at(i).pos.x -= 10;
-                break;
-            case RIGHT:
-                _snakeMove.at(i).pos.x += 10;
-                break;
-        }
-        std::cout << "INDEX[" << i << "]: [" << _snakeMove.at(i).pos.x << "] / [" << _snakeMove.at(i).pos.y << "]\n";
-
-        this->gameData.player.tileSet[i].pos = _snakeMove[i].pos;
+    new_head = _snake[0];
+    switch (_direction) {
+        case UP:
+            new_head.y -= SIZE_BORDER;
+            break;
+        case DOWN:
+            new_head.y += SIZE_BORDER;
+            break;
+        case LEFT:
+            new_head.x -= SIZE_BORDER;
+            break;
+        case RIGHT:
+            new_head.x += SIZE_BORDER;
+            break;
     }
+    _snake.insert(_snake.begin(), new_head);
+    _snake.pop_back();
+    for (std::size_t i = 0; i < _snake.size(); ++i) {
+        this->gameData.player.tileSet[i].pos = _snake[i];
+    }
+}
+
+void Arc::Snake::initEndGame()
+{
+    this->gameData.textSet.clear();
+    this->gameData.tileSet.clear();
+    this->gameData.player.tileSet.clear();
+    // Clear les item
+    this->gameData.player.health = 0;
+    _isEnd = true;
+
+    createText("You loose !" + this->gameData.player.userName, Arc::Pos(400, 50));
+    createText("You score: " + std::to_string(_actualScore), Arc::Pos(600, 50));
+    createText("The highscore is: " + std::to_string(_highScore), Arc::Pos(800, 50));
 }
 
 void Arc::Snake::endTheGame()
 {
-    for (const auto _snakeBody : _snakeMove) {
-        if (_snakeBody.pos.x <= 500 || _snakeBody.pos.x >= 1260 ||
-            _snakeBody.pos.y <= 100 || _snakeBody.pos.y >= 860) {
-                //this->gameData.textSet.clear();
-                //this->gameData.tileSet.clear();
-                //this->gameData.player.health = 0;
-            }
+    if (_isEnd == true) {
+        return;
     }
-}
-
-void Arc::Snake::displayScreenEnd()
-{
-    if (this->gameData.player.health == 0) {
-        // - You are dead
-        // - Username / Actual Score / High Score
-        // - Press Q to Go back Menu
+    for (const auto part : _snake) {
+        if (part.x <= 500 || part.x >= 1260 ||
+            part.y <= 100 || part.y >= 860) {
+                initEndGame();
+        }
     }
 }
 
@@ -306,13 +293,46 @@ void Arc::Snake::checkHighScore()
     // If the actual score > high score ----->>>> write on the file the new high score with the username
 }
 
+bool Arc::Snake::areaRectsInContact(const Rect &rect1, const Rect &rect2)
+{
+    double rect1_right = rect1.x + rect1.w;
+    double rect1_bottom = rect1.y + rect1.h;
+    double rect2_right = rect2.x + rect2.w;
+    double rect2_bottom = rect2.y + rect2.h;
+
+    if (rect1.x > rect2_right || rect2.x > rect1_right || rect1.y > rect2_bottom || rect2.y > rect1_bottom) {
+        return false;
+    }
+    return true;
+}
+
+Rect Arc::Snake::calculateRect(const Pos &pos, double size)
+{
+    Rect rect;
+
+    rect.x = pos.x;
+    rect.y = pos.y;
+    rect.w = size;
+    rect.h = size;
+    return rect;
+}
+
 void Arc::Snake::snakeEatAFood()
 {
-    // If the snake has eat a food
-    _actualScore += 1;
-    // Insert at the size - 1 un élément (Pour préserver la queue)
-    // Il faut insérer un tile
-    // L'emplacement de la pomme doit etre regénérer
+    Pos tail;
+    Rect snakeHeadRect = calculateRect(_snake.at(0), 40);
+    Rect itemRect = calculateRect(this->gameData.item.at(0).pos, 40);
+
+    if (areaRectsInContact(snakeHeadRect, itemRect)) {
+        _actualScore += 1;
+        this->gameData.textSet.at(1).text = "Actual score: " + std::to_string(_actualScore);
+        this->gameData.item.at(0).pos = computeNewSizeItem();
+        this->gameData.item.at(0).tile.pos = this->gameData.item.at(0).pos;
+        tail = this->gameData.player.tileSet.back().pos;
+
+        changePartSnake();
+        createPlayer(tail, Arc::Size(40, 40), SNAKE_TAIL);
+    }
 }
 
 void Arc::Snake::isSnakeFillingAllTheMap()
@@ -325,16 +345,16 @@ const Arc::GameData &Arc::Snake::update(const Arc::Event &event)
     for (const auto &evt : event.eventType) {
         switch (evt) {
             case Arc::EventType::UP:
-                moveSnakeUp();
+                changeDirection(UP);
             break;
             case Arc::EventType::DOWN:
-                moveSnakeDown();
+                changeDirection(DOWN);
             break;
             case Arc::EventType::LEFT:
-                moveSnakeLeft();
+                changeDirection(LEFT);
             break;
             case Arc::EventType::RIGHT:
-                moveSnakeRight();
+                changeDirection(RIGHT);
             break;
             default:
             break;
@@ -342,10 +362,9 @@ const Arc::GameData &Arc::Snake::update(const Arc::Event &event)
     }
     moveNextCase();
     //checkHighScore();
-    //snakeEatAFood();
+    snakeEatAFood();
     //isSnakeFillingAllTheMap();
     endTheGame();
-    //displayScreenEnd();
     return this->gameData;
 }
 
